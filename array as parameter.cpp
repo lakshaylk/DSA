c01567442f1139bc51fa3fa63bb28a9a28592331
Lakshay <121105017+lakshaylk@users.noreply.github.com>
2023-08-04 20:13:40 +0530

    Add files via upload

---
+++ array as parameter.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+void fun(int *A,int n)
+{
+	A[0]=15;
+	for(int i=0;i<n;i++)
+	cout<<A[i]<<endl;
+}
+int main()
+{
+	int A[]={2,4,6,8,10};
+	int n=5;
+	fun(A,n);
+	
+	return 0;
+}
