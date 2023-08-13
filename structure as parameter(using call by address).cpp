442f4f230581fc9ddcfefc6b6840a0e7ccbb5570
Lakshay <121105017+lakshaylk@users.noreply.github.com>
2023-08-14 00:31:21 +0530

    Add files via upload

---
+++ structure as parameter(using call by address).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+struct Rectangle
+{
+	int length;
+	int breadth;
+	
+};
+
+void fun(struct Rectangle *p)
+{
+	p->length=20;
+	cout<<"Length "<<p->length<<endl<<"Breadth "<<p->breadth<<endl;
+}
+
+int main()
+{
+	struct Rectangle r={10,5};
+	fun(&r);
+	
+	printf("length %d \nBreadth %d\n",r.length,r.breadth);
+	return 0;
+}
