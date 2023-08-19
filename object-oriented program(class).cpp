7e5731aa7d936f1d234fa12b1f4874c051fdd9d1
Lakshay <121105017+lakshaylk@users.noreply.github.com>
2023-08-19 18:22:36 +0530

    Add files via upload

---
+++ object-oriented program(class).cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace  std;
+
+class Rectangle
+{
+	public:
+		
+		int length;
+		int breadth;
+
+		void initialise(int l,int b)	
+		{
+			length=l;
+			breadth=b;
+		}
+	
+		int area(){	
+			return length*breadth;
+		}
+	
+		int perimeter(){
+			int p=2*(length+breadth);
+			return p;
+		}
+};
+		
+int main()
+{
+	Rectangle r;
+	
+	int l,b;
+	printf("Enter Length and Breadth");
+	cin>>l>>b;
+	
+	r.initialise(l,b);
+	int a=r.area();
+	int peri=r.perimeter();
+	
+	printf("Area=%d\nPerimeter=%d",a,peri);
+	
+	return 0;
+}
+
+
