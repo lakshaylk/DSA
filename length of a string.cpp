8015f6cf3e7d49986c01554674327817abddca01
Lakshay <121105017+lakshaylk@users.noreply.github.com>
2023-10-18 17:34:21 +0530

    Add files via upload

---
+++ length of a string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char S[]="welcome";
+	int i;
+	for(i=0;S[i]!='\0';i++)
+	{
+	}
+//	printf("Length is %d",i);
+	cout<<"Length is "<<i;	
+	return 0;
+}
