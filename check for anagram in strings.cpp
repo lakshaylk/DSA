3d7402b0bc3fc2551ec50ceee6869b098cb07750
Lakshay <121105017+lakshaylk@users.noreply.github.com>
2023-10-19 17:37:53 +0530

    Add files via upload

---
+++ check for anagram in strings.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	char A[]="decimal";
+	char B[]="medical";
+	int i,H[26]={0};
+	
+	for(i=0;A[i]!='\0';i++)
+	{
+		H[A[i]-97] += 1;
+	}
+	
+	for(i=0;B[i]!='\0';i++)
+	{
+		H[B[i]-97] -= 1;
+		if(H[B[i]-97]<0)
+		{
+			printf("Not Anagram");
+			break;
+		}
+	}
+	
+	if(B[i]=='\0')
+	{
+		printf("Anagram");
+	} 
+	
+	return 0;
+}
