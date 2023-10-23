b4ed3fa2855075de6b358766316901db5457aba1
Lakshay <121105017+lakshaylk@users.noreply.github.com>
2023-10-23 10:09:33 +0530

    Add files via upload

---
+++ permutations of a string.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void perm(char S[],int k)
+{ 
+	static int A[10]={0};
+	static char Res[10];
+	int i;
+	if(S[k]=='\0')
+	{
+		Res[k]='\0';
+		cout<<Res<<endl;
+	}
+	else
+	{
+		for(i=0;S[i]!='\0';i++)
+		{
+			if(A[i]==0)
+			{
+				Res[k]=S[i];
+				A[i]=1;
+				perm(S,k+1);
+				A[i]=0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	char S[]="ABC";
+	perm(S,0);
+	
+	return 0;
+}
